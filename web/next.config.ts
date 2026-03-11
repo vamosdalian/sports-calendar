3abc19fe222ed2938621082c30d2480ca492e4c0
vamosdalian <elve960520@gmail.com>
2026-03-11 09:32:26 +0800

    feat:url

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,41 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: true,
+      },
+      {
+        source: "/:lang(en|zh)",
+        destination: "/:lang/index.html",
+        permanent: true,
+      },
+      {
+        source: "/:lang(en|zh)/:sport/:league/:season",
+        destination: "/:lang/:sport/:league/:season/index.html",
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+      },
+      {
+        source: "/:lang(en|zh)/index.html",
+        destination: "/:lang",
+      },
+      {
+        source: "/:lang(en|zh)/:sport/:league/:season/index.html",
+        destination: "/:lang/:sport/:league/:season",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
